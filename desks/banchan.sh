868388a2cd19df9e9a0eecd132fd44d74988c564
Minjong Chung <mj@tridge.com>
2015-11-16 10:43:06 +0900

    Add `banchan` desk

---
+++ desks/banchan.sh
@@ -0,0 +1,14 @@
+# banchan.sh
+#
+# Description: desk for `banchan` project
+#
+
+PROJECT_PATH=~/workspace/banchan
+
+# Directory
+
+alias banchan='cd $PROJECT_PATH'
+alias b='banchan'
+
+cd $PROJECT_PATH
+workon banchan
