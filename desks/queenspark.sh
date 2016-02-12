05d2d2a6a1bfb2b980d69e1d707afd079086b7f3
Minjong Chung <mj@tridge.com>
2016-02-12 10:42:47 +0900

    Update desk for queenspark

--- desks/queenspark.sh
+++ desks/queenspark.sh
@@ -8,10 +8,17 @@ PROJECT_PATH=~/workspace/queenspark
 # Server
 
 alias dev='ssh deploy@54.178.183.100'
-alias factory='ssh deploy@52.26.114.191'
-alias proxy='ssh deploy@52.24.253.121'
-alias web1='ssh deploy@ec2-52-10-29-127.us-west-2.compute.amazonaws.com'
-alias web2='ssh deploy@ec2-52-25-204-50.us-west-2.compute.amazonaws.com'
+
+alias factory='ssh deploy@52.77.25.219'
+alias proxy='ssh deploy@52.76.238.143'
+alias web1='ssh deploy@ec2-54-251-152-44.ap-southeast-1.compute.amazonaws.com'
+alias web2='ssh deploy@ec2-54-179-140-92.ap-southeast-1.compute.amazonaws.com'
+alias gitlab='ssh -i ~/Key/projectt.pem ubuntu@52.79.104.107'
+
+alias oldfactory='ssh deploy@52.26.114.191'
+alias oldproxy='ssh deploy@52.24.253.121'
+alias oldweb1='ssh deploy@ec2-52-10-29-127.us-west-2.compute.amazonaws.com'
+alias oldweb2='ssh deploy@ec2-52-25-204-50.us-west-2.compute.amazonaws.com'
 
 # Directory
 
