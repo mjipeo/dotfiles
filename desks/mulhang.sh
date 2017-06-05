d92b0f920bb855436ff86f2bd87ce198f4b7ef54
Minjong Chung <mj@tridge.com>
2017-06-05 19:06:26 +0900

    Add mulhang desk

---
+++ desks/mulhang.sh
@@ -0,0 +1,15 @@
+# mulhang.sh
+#
+# Description: desk for `mulhang` project
+#
+
+PROJECT_PATH=~/workspace/mulhang
+
+# Server
+
+alias prod='ssh deploy@13.124.32.126'
+
+# Directory
+
+alias mulhang='cd ${PROJECT_PATH}'
+alias dm='mulhang'
